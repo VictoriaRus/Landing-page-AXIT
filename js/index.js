f8284f4a0c5ec876eb0acd2ea2650a838636dc43
Вика <ruseckaya@cmit.ru>
2022-04-27 14:48:03 +0300

    add: menu-burger

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$().ready(function () {
+
+    $('.burger').click(function () {
+
+        if ($(".burger i").hasClass("fa-solid fa-bars")) {
+            $('.burger i').removeClass('fa-solid fa-bars');
+            $('.burger i').addClass('fa-solid fa-square-xmark');
+        } else {
+            $('.burger i').removeClass('fa-solid fa-square-xmark');
+            $('.burger i').addClass('fa-solid fa-bars');
+        }
+
+        $('.navigation').toggleClass('open');
+        $('.navigation-list').toggleClass('open');
+        $('.navigation-list-item').toggleClass('open-li');
+
+    });
+
+    $('.navigation-list-item').click(function () {
+        if ($(".burger i").hasClass("fa-solid fa-bars")) {
+            $('.burger i').removeClass('fa-solid fa-bars');
+            $('.burger i').addClass('fa-solid fa-square-xmark');
+        } else {
+            $('.burger i').removeClass('fa-solid fa-square-xmark');
+            $('.burger i').addClass('fa-solid fa-bars');
+        }
+        $('.navigation').toggleClass('open');
+        $('.navigation-list').toggleClass('open');
+        $('.navigation-list-item').toggleClass('open-li');
+
+    });
+
+
+});
