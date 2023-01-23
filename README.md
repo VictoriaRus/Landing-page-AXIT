1f5435d4d1e696b25035ba69f3df9c231cac24b0
Вика <ruseckaya@cmit.ru>
2023-01-23 18:52:46 +0300

    add Readme file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LANDING PAGE
-[ View Landing page](https://victoriarus.github.io/FE29-LandingPage-Rusetskaya-Vika/)
+
+[open Landing page](https://victoriarus.github.io/FE29-LandingPage-Rusetskaya-Vika/)
+
+### List of technologies
+- JavaScript (jQuery);
+- CSS;
+
+### Short description
+
+This is a responsive landing page. CSS files contain styles for sections. I used the jQuery library for the burger menu.
+
+The project has an adaptive layout (820px, 768px, 414px - 320px).
+
+### Links to used libraries
+1. [jQuery](https://jquery.com/);
+2. [font-awesome 6](https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css);
--- js/index.js
+++ js/index.js
@@ -1,34 +1,35 @@
 $().ready(function () {
 
-    $('.burger').click(function () {
+    $(".burger").click(function () {
 
         if ($(".burger i").hasClass("fa-solid fa-bars")) {
-            $('.burger i').removeClass('fa-solid fa-bars');
-            $('.burger i').addClass('fa-solid fa-square-xmark');
+            $(".burger i").removeClass("fa-solid fa-bars");
+            $(".burger i").addClass("fa-solid fa-square-xmark");
         } else {
-            $('.burger i').removeClass('fa-solid fa-square-xmark');
-            $('.burger i').addClass('fa-solid fa-bars');
+            $(".burger i").removeClass("fa-solid fa-square-xmark");
+            $(".burger i").addClass("fa-solid fa-bars");
         }
 
-        $('.navigation').toggleClass('open');
-        $('.navigation-list').toggleClass('open');
-        $('.navigation-list-item').toggleClass('open-li');
-        $('body').toggleClass('fixed-page');
+        $(".navigation").toggleClass("open");
+        $(".navigation-list").toggleClass("open");
+        $(".navigation-list-item").toggleClass("open-li");
+        $("body").toggleClass("fixed-page");
     });
 
-    $('.navigation-list-item').click(function () {
+    $(".navigation-list-item").click(function () {
+
         if ($(".burger i").hasClass("fa-solid fa-bars")) {
-            $('.burger i').removeClass('fa-solid fa-bars');
-            $('.burger i').addClass('fa-solid fa-square-xmark');
+            $(".burger i").removeClass("fa-solid fa-bars");
+            $(".burger i").addClass("fa-solid fa-square-xmark");
         } else {
-            $('.burger i').removeClass('fa-solid fa-square-xmark');
-            $('.burger i').addClass('fa-solid fa-bars');
+            $(".burger i").removeClass("fa-solid fa-square-xmark");
+            $(".burger i").addClass("fa-solid fa-bars");
         }
-        $('.navigation').toggleClass('open');
-        $('.navigation-list').toggleClass('open');
-        $('.navigation-list-item').toggleClass('open-li');
-        $('body').toggleClass('fixed-page');
+        
+        $(".navigation").toggleClass("open");
+        $(".navigation-list").toggleClass("open");
+        $(".navigation-list-item").toggleClass("open-li");
+        $("body").toggleClass("fixed-page");
     });
-
 
 });
